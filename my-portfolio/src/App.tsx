92735974f909a4187bf2b93a4fed808e72eae963
Chamith Dilshan Tharuka <dilshanchamith99@gmail.com>
2025-11-12 23:41:01 +0530

    Hero section added

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import '../src/components/Navbar.tsx'
 import Navbar from '../src/components/Navbar.tsx';
+import Hero from '../src/components/Hero.tsx';
 
 const App = () => (
     <>
-    <h1>YEAH!!!</h1>
     <Navbar/>
+    <Hero/>
     </>
 );
 
