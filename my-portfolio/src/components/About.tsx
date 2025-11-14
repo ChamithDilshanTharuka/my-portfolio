802b5f54c075d3f617f624e236d9ec0c0b923872
Chamith Dilshan Tharuka <dilshanchamith99@gmail.com>
2025-11-14 22:50:27 +0530

    About Me component added

---
+++ my-portfolio/src/components/About.tsx
@@ -0,0 +1,10 @@
+import { Code2, Rocket, Zap } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
