d7b3221ba859d13af0b4d816ddeef2ea0c1069bd
Chamith Dilshan Tharuka <dilshanchamith99@gmail.com>
2025-11-15 21:04:55 +0530

    Skills component added

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import Skills from './components/Skills'
 
 const App = () => (
     <>
     <Navbar/>
     <Hero/>
     <About/>
+    <Skills/>
     </>
 );
 
